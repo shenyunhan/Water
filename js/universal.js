98c0d353b20493edd0bbcfc10f8814d2fbb72185
Shannon2016 <shannonsn@qq.com>
2018-11-18 20:28:11 +0800

    2018/11/18 20:28

---
+++ js/universal.js
@@ -0,0 +1,19 @@
+function changesize_text(classname, rate)
+{
+    var scale = $(window).width() / rate;
+    var elem = document.getElementsByClassName(classname);
+    for(var i = 0; i < elem.length; i ++)
+        elem[i].style.fontSize = scale + 'px';
+}
+
+function changesize_img(classname, w, h)
+{
+    var width = ($(window).width() / w) + 'px';
+    var height = ($(window).height() / h) + 'px';
+    var elem = document.getElementsByClassName(classname);
+    for (var i = 0; i < elem.length; i++)
+    {
+        elem[i].style.width = width;
+        elem[i].style.height = height;
+    }
+}
